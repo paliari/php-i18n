a8ab28f89a59b2a0adaf216423e4cff8e27dfbf0
Daniel Fernando Lourusso <dflourusso@gmail.com>
2016-09-14 18:54:08 -0300

    add tests to i18n

--- tests/I18nTest.php
+++ tests/I18nTest.php
@@ -1,5 +1,6 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use Paliari\I18n;
 
 /**
  * Class I18nTest
@@ -7,9 +8,55 @@ use PHPUnit\Framework\TestCase;
 class I18nTest extends TestCase
 {
 
-    public function testShouldBeTrue()
+    protected $i18n;
+
+    public function __construct()
     {
-        $this->assertTrue(true);
+        $this->i18n = I18n::instance();
     }
 
+    public function testCurrentLocaleExists()
+    {
+        $this->assertEquals('pt-BR', $this->i18n->getCurrentLocale());
+    }
+
+    public function testSetCurrentLocale()
+    {
+        $this->i18n->setCurrentLocale('en');
+        $this->assertEquals('en', $this->i18n->getCurrentLocale());
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testSetCurrentLocaleToBlank()
+    {
+        $this->i18n->setCurrentLocale('');
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testWithoutConfigure()
+    {
+        $this->i18n->hum('hello');
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testSetLocalesPathToBlank()
+    {
+        $this->i18n->setLocalesPath('');
+    }
+
+    public function testSetLocalesPathAndTranslate()
+    {
+        $this->i18n->setLocalesPath(__DIR__ . '/locale_examples');
+        $this->assertEquals('Hello I18n', $this->i18n->hum('hello'));
+        $this->assertEquals('Unauthorized', $this->i18n->hum_error_message('unauthorized'));
+        $this->i18n->setCurrentLocale('pt-BR');
+        $this->assertEquals('Olá I18n', $this->i18n->hum('hello'));
+        $this->assertEquals('Não autorizado', $this->i18n->hum_error_message('unauthorized'));
+    }
 }
