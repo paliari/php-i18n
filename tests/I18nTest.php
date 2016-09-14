fa628502b490f7db6c55ae0e76a7259e4d399deb
Daniel Fernando Lourusso <dflourusso@gmail.com>
2016-09-14 17:12:37 -0300

    add the first test

--- tests/I18nTest.php
+++ tests/I18nTest.php
@@ -1,6 +1,15 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class I18nTest
+ */
 class I18nTest extends TestCase
 {
+
+    public function testShouldBeTrue()
+    {
+        $this->assertTrue(true);
+    }
+
 }
